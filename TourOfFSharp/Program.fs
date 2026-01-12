02546741a1e665dfff2b56fed20ad789aa15c9a5
Rafael Barni Ritter <rafaritter44@hotmail.com>
2026-01-11 21:36:20 -0300

    Implement Immutability module

--- TourOfFSharp/Program.fs
+++ TourOfFSharp/Program.fs
@@ -14,3 +14,12 @@
             2.0*x*x + x/5.0 - 37.0
     let result3 = f3 (6.5 + 4.5)
     printfn $"result3: %f{result3}"
+
+module Immutability =
+    let number = 2
+    printfn $"number: {number}"
+
+    let mutable otherNumber = 2
+    printfn $"otherNumber: {otherNumber}"
+    otherNumber <- otherNumber + 1
+    printfn $"otherNumber after mutation: {otherNumber}"
