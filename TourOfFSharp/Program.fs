67b864f8b0af30b5c20dc0c67fa47e5392838f33
Rafael Barni Ritter <rafaritter44@hotmail.com>
2026-01-11 21:27:22 -0300

    Add one more basic function

--- TourOfFSharp/Program.fs
+++ TourOfFSharp/Program.fs
@@ -6,3 +6,11 @@
     let f2 (x:int) = 2*x*x - x/5 + 3
     let result2 = f2 (7 + 4)
     printfn $"result2: %d{result2}"
+
+    let f3 x =
+        if x < 100.0 then
+            2.0*x*x - x/5.0 + 3.0
+        else
+            2.0*x*x + x/5.0 - 37.0
+    let result3 = f3 (6.5 + 4.5)
+    printfn $"result3: %f{result3}"
