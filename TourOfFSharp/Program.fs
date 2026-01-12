8b1d0bf503eec00d95d64da88017a302d4c798d4
Rafael Barni Ritter <rafaritter44@hotmail.com>
2026-01-11 21:24:45 -0300

    Implement BasicFunctions

--- TourOfFSharp/Program.fs
+++ TourOfFSharp/Program.fs
@@ -1,2 +1,8 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿module BasicFunctions =
+    let f1 x = x*x + 3
+    let result1 = f1 4573
+    printfn $"result1: %d{result1}"
+
+    let f2 (x:int) = 2*x*x - x/5 + 3
+    let result2 = f2 (7 + 4)
+    printfn $"result2: %d{result2}"
