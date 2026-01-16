12042db7771ecd3652ac2b8df85d87ee984ff645
Rafael Barni Ritter <rafaritter44@hotmail.com>
2026-01-16 17:09:55 -0300

    Implement recursive discriminated unions POC

--- TourOfFSharp/Program.fs
+++ TourOfFSharp/Program.fs
@@ -317,3 +317,22 @@ module SingleCaseDiscriminatedUnions =
     let unwrapSSN (SSN s) = s
 
     printfn $"Address: {address |> unwrapAddress}, Name: {name |> unwrapName}, and SSN: {ssn |> unwrapSSN}"
+
+module RecursiveDiscriminatedUnions =
+    type BST<'T> =
+        | Empty
+        | Node of value:'T * left: BST<'T> * right: BST<'T>
+    let rec exists item bst =
+        match bst with
+        | Empty -> false
+        | Node (x, left, right) ->
+            if item = x then true
+            elif item < x then (exists item left)
+            else (exists item right)
+    let rec insert item bst =
+        match bst with
+        | Empty -> Node(item, Empty, Empty)
+        | Node (x, left, right) as node ->
+            if item = x then node
+            elif item < x then Node (x, insert item left, right)
+            else Node (x, left, insert item right)
