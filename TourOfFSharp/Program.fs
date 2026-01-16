03576c62cdc11c8d21ba734249f34af862429585
Rafael Barni Ritter <rafaritter44@hotmail.com>
2026-01-16 16:51:29 -0300

    Implement discriminated unions POC

--- TourOfFSharp/Program.fs
+++ TourOfFSharp/Program.fs
@@ -259,3 +259,46 @@ module RecordTypes =
         Verified = false
     }
     printfn $"Alf's contact card struct:\n{contactStruct}"
+
+module DiscriminatedUnions =
+    type Suit =
+        | Hearts
+        | Clubs
+        | Diamonds
+        | Spades
+    type Rank =
+        | Value of int
+        | Ace
+        | King
+        | Queen
+        | Jack
+        static member GetAllRanks() =
+            [ yield Ace
+              for i in 2..10 do yield Value i
+              yield Jack
+              yield Queen
+              yield King ]
+    type Card = { Suit: Suit; Rank: Rank }
+    let fullDeck =
+        [ for suit in [ Hearts; Diamonds; Clubs; Spades ] do
+            for rank in Rank.GetAllRanks() do
+                yield { Suit=suit; Rank=rank } ]
+    let showPlayingCard (c: Card) =
+        let rankString =
+            match c.Rank with
+            | Ace -> "Ace"
+            | King -> "King"
+            | Queen -> "Queen"
+            | Jack -> "Jack"
+            | Value n -> string n
+        let suitString =
+            match c.Suit with
+            | Clubs -> "clubs"
+            | Diamonds -> "diamonds"
+            | Spades -> "spades"
+            | Hearts -> "hearts"
+        rankString + " of " + suitString
+    let printAllCards() =
+        for card in fullDeck do
+            printfn $"{showPlayingCard card}"
+    printAllCards()
